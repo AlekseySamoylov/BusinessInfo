80d8043e04e478e60b7c8c8f19c4fcd07d7029fc
AlekseySamoylov <alekseysamoylov89@gmail.com>
2016-10-01 13:53:09 +0300

    Test commit

--- BusinessInfo/ViewController.swift
+++ BusinessInfo/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var testLabel: UILabel!
 
+    @IBOutlet weak var testField: UITextField!
+    
+    @IBOutlet weak var testButton: UIButton!
+    
+    // Разблокирует кнопку если текст в поле присутствует
+    @IBAction func checkTestFieldNotEmpty(_ sender: UITextField) {
+        if (testField.text?.isEmpty)! {
+            testButton.isEnabled = false
+        } else {
+            testButton.isEnabled = true
+        }
+    }
+    
+    // Устанавливает Label text
+    @IBAction func writeTestField(_ sender: UIButton) {
+        testLabel.text = testField.text?.capitalized
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
